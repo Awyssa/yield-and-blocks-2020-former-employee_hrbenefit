3ed0771d6ef420d400b042085a54b034da8bbcfd
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 12:24:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,15 +12,3 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
     puts "Hi, #{name}"
   end
 end
-
-
-
-
-
-
-
-# ["Tim", "Tom", "Jim"].each do |name|
-#   if name.start_with?("T")
-#     puts "Hi, #{name}"
-#   end
-# end
