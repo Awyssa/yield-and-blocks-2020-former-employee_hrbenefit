dcb4fc29cfb16744b5b661fe4ef2b171ab499192
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 12:31:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,13 +2,14 @@ def hello_t(array)
 
   if block_given?
     i = 0
-
-  while i < array.length
-    yield array[i]
-    i = i + 1
-
-  end
-  return array
+    while i < array.length
+       yield array[i]
+       i = i + 1
+     end
+     return array
+   else
+     puts "Hey! No bloack was given!"
+   end
 end
 
 
