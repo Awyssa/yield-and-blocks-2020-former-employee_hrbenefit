aafc8ae446b11c638f445b7562dfe6efd96753d5
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 12:15:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,23 @@
-def hello_t
+def hello_t(array)
+  1 = 0
+
+  while i < array.length
+    i = i + 1
+  end
+  
 end
 
 # call your method here!
 
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+
+
+
+
+
+
+
+# ["Tim", "Tom", "Jim"].each do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
