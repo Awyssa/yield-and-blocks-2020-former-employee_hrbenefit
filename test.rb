814032198e8447994413dd8a0d0ec61a7f232800
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 11:38:58 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,16 @@
+
+candy = [2,4,6,8]
+
+candy_times_three = candy.map { |num| num * 3 }
+
+puts candy_times_three
+
+def introduction(name)
+  yield(name)
+  puts "nice to meet you"
+  yield("Amanda")
+  yield(name)
+  yield("John")
+end
+
+introduction("Ernest") {|n| puts "My name is #{n}!"}
