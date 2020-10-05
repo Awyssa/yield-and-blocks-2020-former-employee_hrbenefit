79c77f1d21f314f706eaac355a49411f5ef7d005
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 11:41:32 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,14 +3,14 @@ candy = [2,4,6,8]
 
 candy_times_three = candy.map { |num| num * 3 }
 
-puts candy_times_three
+candy_times_three
 
-def introduction(name)
-  yield(name)
-  puts "nice to meet you"
-  yield("Amanda")
-  yield(name)
-  yield("John")
-end
-
-introduction("Ernest") {|n| puts "My name is #{n}!"}
+# def introduction(name)
+#   yield(name)
+#   puts "nice to meet you"
+#   yield("Amanda")
+#   yield(name)
+#   yield("John")
+# end
+#
+# introduction("Ernest") {|n| puts "My name is #{n}!"}
